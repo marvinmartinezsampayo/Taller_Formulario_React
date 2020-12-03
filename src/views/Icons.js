26d8e47a134976589f08fdbd6b1b89acf5272e95
Marvin Martinez <marvin.martinez@correo.policia.gov.co>
2020-12-02 20:10:41 -0500

    Creando la rama y se desarrolla la tarea N°1

--- src/views/Icons.js
+++ src/views/Icons.js
@@ -1,22 +1,6 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
+import DataTable, { createTheme } from 'react-data-table-component';
+import { Button } from 'react-bootstrap';
 
 // reactstrap components
 import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
@@ -26,13 +10,87 @@ class Icons extends React.Component {
     return (
       <>
         <div className="content">
-          <Row>
-            <h1>Aca debes visualizar los datos en una tabla con un boton para eliminar los registros</h1>
-          </Row>
+          <Col md="12">
+            <Card>
+              <CardBody>
+                <div >
+
+                  <MyComponent></MyComponent>
+
+
+
+
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
         </div>
       </>
     );
   }
 }
+
+
+
+
+//const data = [{ id: 1, title: 'Conan the Barbarian', year: '1982' },{ id: 2, title: 'Ñonan the Barbarian', year: '1982' }];}
+class MyComponent extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    }
+  }
+
+  render() {
+    // console.log(this.state.data);
+    return (
+      <DataTable
+        title="Title"
+        columns={[
+          {
+            name: 'Nombres y Apellidos',
+            selector: 'name',
+            sortable: true,
+          },
+          {
+            name: 'Profesión',
+            selector: 'profession',
+            sortable: true,
+          },
+          {
+            name: 'Prefesión',
+            selector: 'profession',
+            sortable: true,
+            right: true,
+            cell: (row) => {
+              // console.log('Row: ', row);
+              return (
+                <Button variant="warning" onClick={()=>{this.enviar(row)}}>Primary</Button>
+               
+              )
+            }
+          },
+        ]}
+        data={this.state.data}
+      />
+
+    )
+  }
+
+  enviar = (developer) => {
+    console.log(developer)
+  }  
+
+
+  //Consumir la API
+  componentDidMount() {
+    fetch('https://dinae-developers-api.herokuapp.com/developers')
+      .then(result => result.json())
+      .then(items => this.setState({ data: items }))
+  };
+
+};
 
 export default Icons;
