dc55fa1c0771e77023cd312ce159d784d2eb8d22
OFITE - JUAN CARLOS CIFUENTES MURCIA <JUAN.CIFUENTES@policia.gov.co>
2020-12-07 11:36:37 -0500

    Formulario inicial

--- src/views/Registro.js
+++ src/views/Registro.js
@@ -25,7 +25,8 @@ import {
   Marker,
 } from "react-google-maps";
 // reactstrap components
-import {  Button,
+import {
+  Button,
   Card,
   CardHeader,
   CardBody,
@@ -35,7 +36,10 @@ import {  Button,
   Form,
   Input,
   Row,
-  Col, } from "reactstrap";
+  Col,
+} from "reactstrap"
+var DatePicker = require("reactstrap-date-picker");
+
 
 const MapWrapper = withScriptjs(
   withGoogleMap((props) => (
@@ -182,151 +186,239 @@ const MapWrapper = withScriptjs(
   ))
 );
 
+
 class Map extends React.Component {
   render() {
-    return (      
-        <>
-          <div className="content">
-            <Row>              
-              <Col md="12">
-                <Card className="card-user">
-                  <CardHeader>
-                    <CardTitle tag="h5" style={{textAlign:"center"}}>Developer registration</CardTitle>
-                  </CardHeader>
-                  <CardBody>
-                    <Form onSubmit={this.enviarDatos}>
-                      <Row>
-                        <Col className="pr-1" md="5">
-                          <FormGroup>
-                            <label>Company (disabled)</label>
-                            <Input
-                              defaultValue="Creative Code Inc."
-                              disabled
-                              placeholder="Company"
-                              type="text"
-                              name="Company"
-                             // onChange={handleInputChange}
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="px-1" md="3">
-                          <FormGroup>
-                            <label>Username</label>
-                            <Input
-                              defaultValue="michael23"
-                              placeholder="Username"
-                              type="text"
-                              name="Name"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pl-1" md="4">
-                          <FormGroup>
-                            <label htmlFor="exampleInputEmail1">
-                              Email address
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col md="12">
+              <Card className="card-user">
+                <CardHeader>
+                  <CardTitle tag="h5" style={{ textAlign: "center" }}>Developer registration</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Form onSubmit={this.enviarDatos}>
+                    <Row>
+                      <Col className="pr-1" md="5">
+                        <FormGroup>
+                          <label>Company (disabled)</label>
+                          <Input
+                            defaultValue="Creative Code Inc."
+                            disabled
+                            placeholder="Company"
+                            type="text"
+                            name="Company"
+                          // onChange={handleInputChange}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-1" md="3">
+                        <FormGroup>
+                          <label>Username</label>
+                          <Input
+                            defaultValue="michael23"
+                            placeholder="Username"
+                            type="text"
+                            name="Name"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="4">
+                        <FormGroup>
+                          <label htmlFor="exampleInputEmail1">
+                            Email address
                             </label>
-                            <Input placeholder="Email" type="email"  name="Email"/>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>First Name</label>
-                            <Input
-                              defaultValue="Chet"
-                              placeholder="Company"
-                              type="text"
-                              name="Primer_Name"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pl-1" md="6">
-                          <FormGroup>
-                            <label>Last Name</label>
-                            <Input
-                              defaultValue="Faker"
-                              placeholder="Last Name"
-                              type="text"
-                              name="Segundo_Name"
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md="12">
-                          <FormGroup>
-                            <label>Address</label>
-                            <Input
-                              defaultValue="Melbourne, Australia"
-                              placeholder="Home Address"
-                              type="text"
-                              name="Direccion"
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col className="pr-1" md="4">
-                          <FormGroup>
-                            <label>City</label>
-                            <Input
-                              defaultValue="Melbourne"
-                              placeholder="City"
-                              type="text"
-                              name="Ciudad"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="px-1" md="4">
-                          <FormGroup>
-                            <label>Country</label>
-                            <Input
-                              defaultValue="Australia"
-                              placeholder="Country"
-                              type="text"
-                              name="Pais"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pl-1" md="4">
-                          <FormGroup>
-                            <label>Postal Code</label>
-                            <Input placeholder="ZIP Code" type="number"  name="Indicativo"/>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md="12">
-                          <FormGroup>
-                            <label>About Me</label>
-                            <Input
-                              type="textarea"
-                              defaultValue="Oh so, your weak rhyme You doubt I'll bother, reading into it"
-                              name="Descripcion"
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <div className="update ml-auto mr-auto">
-                          <Button
-                            className="btn-round"
-                            color="primary"
-                            type="submit"
-                          >
-                            Capturar datos
+                          <Input placeholder="Email" type="email" name="Email" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="6">
+                        <FormGroup>
+                          <label>First Name</label>
+                          <Input
+                            defaultValue="Chet"
+                            placeholder="Company"
+                            type="text"
+                            name="Primer_Name"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="6">
+                        <FormGroup>
+                          <label>Last Name</label>
+                          <Input
+                            defaultValue="Faker"
+                            placeholder="Last Name"
+                            type="text"
+                            name="Segundo_Name"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          <label>Address</label>
+                          <Input
+                            defaultValue="Melbourne, Australia"
+                            placeholder="Home Address"
+                            type="text"
+                            name="Direccion"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="4">
+                        <FormGroup>
+                          <label>City</label>
+                          <Input
+                            defaultValue="Melbourne"
+                            placeholder="City"
+                            type="text"
+                            name="Ciudad"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-1" md="4">
+                        <FormGroup>
+                          <label>Country</label>
+                          <Input
+                            defaultValue="Australia"
+                            placeholder="Country"
+                            type="text"
+                            name="Pais"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="4">
+                        <FormGroup>
+                          <label>Postal Code</label>
+                          <Input placeholder="ZIP Code" type="number" name="Indicativo" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          <label>About Me</label>
+                          <Input
+                            type="textarea"
+                            defaultValue="Oh so, your weak rhyme You doubt I'll bother, reading into it"
+                            name="Descripcion"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div className="update ml-auto mr-auto">
+                        <Button
+                          className="btn-round"
+                          color="primary"
+                          type="submit"
+                        >
+                          Capturar datos
                           </Button>
-                        </div>
-                      </Row>
-                    </Form>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </div>
-        </>
-      );
+                      </div>
+                    </Row>
+                  </Form>
+
+
+
+
+                  <Form onSubmit={this.enviarDatos}>
+                    <Row>
+                      <Col className="pr-1" md="6">
+                        <Row>
+                          <Col className="pr-1" md="12">
+                            <FormGroup>
+                              <label>Name</label>
+                              <Input
+                                placeholder="Name"
+                                type="text"
+                                name="Name"
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col className="pr-1" md="12">
+                            <FormGroup>
+                              <label>Profetion</label>
+                              <Input
+                                placeholder="Profetion"
+                                type="text"
+                                name="profetion"
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col className="pr-1" md="12">
+                            <FormGroup>
+                              <label>Birhdate</label>
+                              <DatePicker id="example-datepicker"
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col className="pr-1" md="12">
+                            <FormGroup>
+                              <label>Join Date</label>
+                              <DatePicker id="example-datepicker"
+                              />multi-sele
+                            </FormGroup>
+                          </Col>
+                          
+                        </Row>
+
+                      </Col>
+
+                      <Col className="pr-1" md="6">
+                        <Row>
+                          <Col className="pr-1" md="5">
+                            <FormGroup>
+                              <label>Join Date</label>
+                              <label>Company (disabled)</label>
+                              <Input
+                                defaultValue="Creative Code Inc."
+                                disabled
+                                placeholder="Company"
+                                type="text"
+                                name="Company"
+                              // onChange={handleInputChange}
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col className="px-1" md="3">
+                            <FormGroup>
+                              <label>Username</label>
+                              <Input
+                                defaultValue="michael23"
+                                placeholder="Username"
+                                type="text"
+                                name="Name"
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col className="pl-1" md="4">
+                            <FormGroup>
+                              <label htmlFor="exampleInputEmail1">
+                                Email address
+                            </label>
+                              <Input placeholder="Email" type="email" name="Email" />
+                            </FormGroup>
+                          </Col>
+                        </Row>
+
+                      </Col>
+                    </Row>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
   }
 }
 
